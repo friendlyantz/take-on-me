59226b00bfa6718ae132e2bacb0924b373e1bd66
anton <friendlyantz@pm.me>
2025-04-09 00:40:17 +1000

    DB-CONFIG-FIX: add host for kamal deploy

--- db/production.sql
+++ db/production.sql
@@ -1,1 +1,7 @@
-CREATE DATABASE take_on_me_production
+CREATE DATABASE take_on_me_production;
+
+CREATE DATABASE take_on_me_production_cache;
+
+CREATE DATABASE take_on_me_production_queue;
+
+CREATE DATABASE take_on_me_production_cable;
