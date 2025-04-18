359567a04136669616e5e62a81fa67ec485e18f8
anton <friendlyantz@pm.me>
2025-04-18 18:29:46 +1000

    Revert "Reapply "OBESRVABILITY: add opentelemetry-api" and bump gRPC FIX"
    This reverts commit 56cfc01b2776949352d58ec224cbdaa65536ab1e.

--- config/initializers/opentelemetry.rb
+++
@@ -1,8 +0,0 @@
-require "opentelemetry/sdk"
-require "opentelemetry/instrumentation/all"
-require "opentelemetry/exporter/otlp"
-
-OpenTelemetry::SDK.configure do |c|
-  c.service_name = "Take On Me Rails OpenTelemetry"
-  c.use_all # enables all instrumentation!
-end
