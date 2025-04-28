17302b805836ded235013b073bf3ae754ac9f752
anton <friendlyantz@pm.me>
2025-04-28 21:50:47 +1000

    Update icons and enable pwa icons

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
   end
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "manifest" => "rails/pwa#manifest", :as => :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
