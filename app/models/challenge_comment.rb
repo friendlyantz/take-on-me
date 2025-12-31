b13953a6f686b5c82efd9b7c547f4d0081794abb
Anton <70934030+friendlyantz@users.noreply.github.com>
2025-12-31 12:29:03 +1100

    limit comment 1 per particip[ant per day

--- app/models/challenge_comment.rb
+++ app/models/challenge_comment.rb
@@ -9,6 +9,7 @@ class ChallengeComment < ApplicationRecord
   has_many :liking_users, through: :challenge_comment_likes, source: :user
 
   validates :message, presence: true
+  validate :one_comment_per_day_per_participant
 
   broadcasts_to :challenge_story, action: :prepend
   default_scope { order(created_at: :asc) } # Oldest first
@@ -20,5 +21,20 @@ class ChallengeComment < ApplicationRecord
 
   def like_count
     challenge_comment_likes.count
+  end
+
+  private
+
+  def one_comment_per_day_per_participant
+    return if challenge_participant.blank?
+
+    existing_comment_today = ChallengeComment.unscoped.exists?(
+      challenge_participant_id: challenge_participant_id,
+      created_at: Time.zone.today.all_day
+    )
+
+    if existing_comment_today && new_record?
+      errors.add(:base, "You can only check in once per day")
+    end
   end
 end
