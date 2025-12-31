223d1bd1117a65400bffdebe3b0795c7a70a0cec
Anton <70934030+friendlyantz@users.noreply.github.com>
2026-01-01 10:34:36 +1100

    counter cache redundant removal

--- app/models/challenge_comment.rb
+++ app/models/challenge_comment.rb
@@ -7,7 +7,7 @@ class ChallengeComment < ApplicationRecord
   belongs_to :challenge_participant, counter_cache: true
   belongs_to :challenge_story, counter_cache: true
   has_one_attached :photo
-  has_many :challenge_comment_likes, dependent: :destroy, counter_cache: true
+  has_many :challenge_comment_likes, dependent: :destroy
   has_many :liking_users, through: :challenge_comment_likes, source: :user
 
   # Validations
