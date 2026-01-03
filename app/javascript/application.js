62e3ae4d095402fff9a62c01eba46a239768eb2e
Anton <70934030+friendlyantz@users.noreply.github.com>
2026-01-03 21:21:53 +1100

    FEAT: PWA service worker added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -20,4 +20,21 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
+
+function registerServiceWorker() {
+  console.log('Registering service worker...');
+
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('Service worker registered:', registration);
+    }).catch((error) => {
+      console.error('Service worker registration failed:', error);
+    });
+  });
+}
+
+if ('serviceWorker' in navigator) {
+  registerServiceWorker();
+}
+
 Rails.start();
